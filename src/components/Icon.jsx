e484d556a31f7cb4f546f479ff4dc5bb19f22aed
tulzlol <tulo.nv@gmail.com>
2022-11-09 18:55:49 -0300

    a

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCartShopping, faExclamationTriangle} from '@fortawesome/free-solid-svg-icons';
 
-function Icon (name) {
+const Icon = (name) => {
         switch (name) {
             case "cart":
                     return (<FontAwesomeIcon icon={faCartShopping} />)
